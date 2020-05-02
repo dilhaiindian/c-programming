abaf04580999053a5995cdb6259ed2f3cef531b8
Coursera Learner <nobody@nowhere.nul>
2020-05-02 09:31:12 +0000

    Did Retirement Problem

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct _retire_info{
+struct _retire_info{
   int months;
   double contribution;
   double rate_of_return;
-}retire_info;
+};
+typedef struct _retire_info retire_info;
 
 double balance_calculator(double balance, retire_info record){
   balance += balance * record.rate_of_return;

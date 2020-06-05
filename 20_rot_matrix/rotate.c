21042349415ffdf2ca9be905f2afe4560d4b951a
Coursera Learner <nobody@nowhere.nul>
2020-06-05 14:09:32 +0000

    Matrix Rotation Complete

--- 20_rot_matrix/rotate.c
+++ 20_rot_matrix/rotate.c
@@ -9,6 +9,13 @@ for (int i = 0; i < 10 ; i++)   {
     matrix[j][i]=temp;
   }
  }
+ for (int i = 0; i < 10 ; i++){
+   for (int j = 0; j < 5; j++){
+     char  temp =matrix[i][j];
+     matrix[i][j]=matrix[i][9-j];
+     matrix[i][9-j]=temp;
+   }
+ }
  return;
 }
 

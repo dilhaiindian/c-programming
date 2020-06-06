2f416fc9380ccb80fc041a5cc96f9f0e9dd2360d
Coursera Learner <nobody@nowhere.nul>
2020-06-06 20:28:20 +0000

    Did Assignment 23

---
+++ 23_power_rec/power.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned y);
+
+unsigned power(unsigned x, unsigned y) {
+  // special case
+  if (x == 0 && y == 0) {
+    return 1;
+  }
+  if (x == 0) {
+    return 0;
+  }
+  // base case
+  if (y == 0) {
+    return 1;
+  }
+  // recursion
+  return x * power(x, y - 1);
+}
